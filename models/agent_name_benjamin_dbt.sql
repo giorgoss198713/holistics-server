56c1333291d3bd89204415e6c90d01bf3f2f6696
george.karageorgiou@gmail.com <george.karageorgiou@gmail.com>
2024-03-27 12:33:16 +0000

    adding transformation for agent name benjamin model

---
+++ models/agent_name_benjamin_dbt.sql
@@ -0,0 +1,10 @@
+SELECT DISTINCT
+u.username as agent_username,
+bn.agent_name,
+twb.agent_id_brand_final
+FROM
+  sales_marketing.transactions_with_brand_dbt twb
+  INNER JOIN sales_marketing.admin_users_dbt u on CONCAT_WS('_',u.id,u.brand_name) = twb.agent_id_brand_final
+  INNER JOIN sales_marketing.benjamin_report_usernames_sheet1 bn ON bn.agent_username=u.username
+  WHERE
+  u.username  in (select agent_username from sales_marketing.benjamin_report_usernames_sheet1)
