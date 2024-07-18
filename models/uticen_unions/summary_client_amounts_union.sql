31f2b1551f5627c5bf3045808d7ae69c3d612c89
george.karageorgiou@gmail.com <george.karageorgiou@gmail.com>
2024-07-18 07:57:48 +0000

    changing models for marketing email surves (summary client amount union)

--- models/uticen_unions/summary_client_amounts_union.sql
+++ models/uticen_unions/summary_client_amounts_union.sql
@@ -1,5 +1,7 @@
 select
-login, client_id, client_id_brand, brand_name, ftd_date, deposits, withdrawals,
+client_id_brand, pool, brand_name, ftd_date, 
+deposit_last_day, deposit_five_days_before, client_in_email_list_cat3,
+deposits, withdrawals,
 net_deposits, deposits_ex_ftd, withdrawals_ex_ftd, net_deposits_ex_ftd,
 deposits_count_prev_day, deposits_minus_one_interval_day,
 deposits_minus_one_interval_month, deposits_count_prev_month,
