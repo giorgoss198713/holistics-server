b187007c5d66f38055d2ce3ea9e2d0abbc5d8ded
george.karageorgiou@gmail.com <george.karageorgiou@gmail.com>
2024-08-20 09:33:40 +0000

    modifying agent name benjamin dbt source excel file and adding agent name benjamin union

--- models/agent_name_benjamin_dbt.sql
+++ models/agent_name_benjamin_dbt.sql
@@ -5,6 +5,6 @@ twb.agent_id_brand_final
 FROM
   sales_marketing.transactions_with_brand_dbt twb
   INNER JOIN sales_marketing.admin_users_dbt u on CONCAT_WS('_',u.id,u.brand_name) = twb.agent_id_brand_final
-  INNER JOIN sales_marketing.benjamin_report_usernames_sheet1 bn ON bn.agent_username=u.username
+  INNER JOIN sales_marketing.benjamin_report_usernames bn ON bn.agent_username=u.username
   WHERE
-  u.username  in (select agent_username from sales_marketing.benjamin_report_usernames_sheet1)
+  u.username  in (select agent_username from sales_marketing.benjamin_report_usernames)
